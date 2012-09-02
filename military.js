9f937c8522182c34d3933c8d5b55d74e0dc3df31
Mostafa Eweda <mostafa.eweda17@gmail.com>
2012-09-02 17:26:49 +0200

    Explain rest days

--- military.js
+++ military.js
@@ -4,7 +4,9 @@ var Twit = require("twit");
 var T = new Twit(require("./config"));
 
 var days = Math.floor((new Date(2012, 10, 25) - new Date()) / (1000 * 60 * 60 * 24));
-var serviceDays = days - (5 + Math.floor(days/7) + 4);
+// 5: Big Muslim Feast
+// 4: mo5atat ra7at
+var serviceDays = days - (5 + 4 + Math.floor(days/7));
 
 var status = "- " + days + " days to finish\n- " + serviceDays + " service days left\n #MilitaryService #AUTOMATED";
 
